10cd4b18136286af10786722feabb1d30756b8d5
Mesaque Silva <mesaque.s.silva@gmail.com>
2015-06-20 22:02:28 -0300

    finalized api basis

--- scan.sh
+++ scan.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 basedir=$(dirname $0);
+filter_name=$( echo ${1} | sed 's~http[s]*://~~g' | sed 's=/[^/]*$==;s/\.$//' )
 [ "$1" ] || {
 	echo "Missing Web site parameter";
 	exit 1;
-} 
+}
+
+[ ! -d $basedir/dump ] && mkdir -p $basedir/dump
 
 source $basedir/app.conf
 
-ruby $wp_scan_path --url $1 --batch --random-agent --log &> $basedir/dump/${1}.txt
+ruby $wp_scan_path --url $1 --batch --random-agent --log &> $basedir/dump/${filter_name}.txt
 
-problems_found=$( cat $basedir/dump/${1}.txt | grep '[!]' | sed 's/[^ ]* //' | tr '&' '&amp;' | tr '<' '&lt;' | tr '>' '&gt;' | tr ' ' _  )
+problems_found=$( cat $basedir/dump/${filter_name}.txt | grep '[!]' | sed 's/[^ ]* //' | tr '&' '&amp;' | tr '<' '&lt;' | tr '>' '&gt;' | tr ' ' _  )
 
 $basedir/slack_notification "WP-Scan of ${1}" "${problems_found}" ${basedir}
